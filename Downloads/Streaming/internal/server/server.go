9821407ff5a465e670777359fe7d651c7e2f7b51
Atul <atul5465king@gmail.com>
2025-10-04 17:25:39 +0530

    server updated

--- Downloads/Streaming/internal/server/server.go
+++ Downloads/Streaming/internal/server/server.go
@@ -1,1 +1,34 @@
 package server
+
+import (
+	"flag"
+	"os"
+	"time"
+)
+var(
+	addr = flag.String("addr,":"",os.Getenv("PORT"),""
+	cert = flag*flag.String("cert","","")
+	key=fl*flag.String("key","","")
+)
+func  Run() error  {
+	flag.Parse()
+	if *addr==":"{
+		*addr=":8080"
+	}
+	// routes
+	app.GET("/",handlers.Welcome)
+	app.GET("/room/create",handlers.RoomCreate)
+	app.GET("/room/:uuid",handlers.Room)
+	app.GET("/room/:uuid/websocket",)
+	app.GET("/room/:uuid/chat",handlers.RoomChat)
+	app.GET("/room/:uuid/chat/websocket",websocket.New(handlers.RoomChatWebsocket))
+	app.GET("/room/:uuid/viewer/websocket",websocket.New(handlers.RoomViewerWebsocket))
+	app.Get("/stream/:ssuid",handlers.Stream)
+	app.Get("/stream/:ssuid/websocket",)
+	app.Get("/stream/:ssuid/chat/websocket",)
+	app.Get("/stream/:ssuid/viewer/websocket",)
+
+}
+
+
+)
